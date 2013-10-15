c642cb4235a542dbe58811cd5c649f7809773ec4
自寒 <zihan.yx@taobao.com>
2013-10-15 16:03:42 +0800

    update

--- 1.0/index.js
+++ 1.0/index.js
@@ -3,7 +3,7 @@
  * @author 自寒<zihan.yx@taobao.com>
  * @module blur
  **/
-KISSY.add(function (S, Node,Base,DD) {
+KISSY.add(function (S, Node,Base) {
     var EMPTY = '';
     var $ = Node.all;
 
@@ -11,6 +11,7 @@ KISSY.add(function (S, Node,Base,DD) {
     var isCached = false;
     var mul_table = [512, 512, 456, 512, 328, 456, 335, 512, 405, 328, 271, 456, 388, 335, 292, 512, 454, 405, 364, 328, 298, 271, 496, 456, 420, 388, 360, 335, 312, 292, 273, 512, 482, 454, 428, 405, 383, 364, 345, 328, 312, 298, 284, 271, 259, 496, 475, 456, 437, 420, 404, 388, 374, 360, 347, 335, 323, 312, 302, 292, 282, 273, 265, 512, 497, 482, 468, 454, 441, 428, 417, 405, 394, 383, 373, 364, 354, 345, 337, 328, 320, 312, 305, 298, 291, 284, 278, 271, 265, 259, 507, 496, 485, 475, 465, 456, 446, 437, 428, 420, 412, 404, 396, 388, 381, 374, 367, 360, 354, 347, 341, 335, 329, 323, 318, 312, 307, 302, 297, 292, 287, 282, 278, 273, 269, 265, 261, 512, 505, 497, 489, 482, 475, 468, 461, 454, 447, 441, 435, 428, 422, 417, 411, 405, 399, 394, 389, 383, 378, 373, 368, 364, 359, 354, 350, 345, 341, 337, 332, 328, 324, 320, 316, 312, 309, 305, 301, 298, 294, 291, 287, 284, 281, 278, 274, 271, 268, 265, 262, 259, 257, 507, 501, 496, 491, 485, 480, 475, 470, 465, 460, 456, 451, 446, 442, 437, 433, 428, 424, 420, 416, 412, 408, 404, 400, 396, 392, 388, 385, 381, 377, 374, 370, 367, 363, 360, 357, 354, 350, 347, 344, 341, 338, 335, 332, 329, 326, 323, 320, 318, 315, 312, 310, 307, 304, 302, 299, 297, 294, 292, 289, 287, 285, 282, 280, 278, 275, 273, 271, 269, 267, 265, 263, 261, 259];
     var shg_table = [9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17, 17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24];
+    var ctx, tempImg;
 
     /**
      * 
@@ -29,21 +30,36 @@ KISSY.add(function (S, Node,Base,DD) {
         _init: function(){
             var self = this;
             if(!canvas.getContext) return;
-            self.container = $(self.get('node'));
-            if(!self.container) return;
             self._render();
         },
         _render: function(){
             var self = this;
             var options = self.getAttrVals();
-            var glue = self.container;
+            var glue = $(options.node);
             var source = $(options.source);
-            var formattedSource = (source.css('backgroundImage')).replace(/"/g, "").replace(/url\(|\)$/ig, "");
+            if(!source.length) return;
+            var sourceimg = source.one('img');
+            if(!sourceimg.length || !sourceimg.attr('src')) return;
+
+            var formattedSource = sourceimg.attr('src').replace(/"/g, "").replace(/url\(|\)$/ig, "");
+
+            $(canvas).css({'width':sourceimg.width(),'height':sourceimg.height(),'position':'absolute','top':'0','left':'0'}).appendTo(source);
             ctx = canvas.getContext('2d');
+            tempImg = new Image();
+            tempImg.onload = function(){
+                canvas.width = tempImg.width;
+                canvas.height = tempImg.height;
+                ctx.clearRect( 0, 0, canvas.width, canvas.height );
+                ctx.drawImage(tempImg, 0, 0);
+                self._stackBlurCanvasRGB(canvas, 0, 0, canvas.width, canvas.height, options.radius);
+            };
+            tempImg.src = formattedSource;
+        
+            /*ctx = canvas.getContext('2d');
             tempImg = new Image();
             tempImg.onload = function () {
                 if(!isCached) {
-                    canvas.style.display = "none";
+                    // canvas.style.display = "none";
                     canvas.width = tempImg.width;
                     canvas.height = tempImg.height;
                     ctx.drawImage(tempImg, 0, 0);
@@ -68,30 +84,32 @@ KISSY.add(function (S, Node,Base,DD) {
                 } else {
                     var blurredData = tempImg.src;
                 }
-                var attachment = source.css('backgroundAttachment');
-                var position = (attachment == 'fixed') ? '' : '-' + ((glue.offset().left) - (source.offset().left) - (options.offset.x)) + 'px -' + ((glue.offset().top) - (source.offset().top) - (options.offset.y)) + 'px';
-                glue.css({
-                    'background-image': 'url("' + blurredData + '")',
-                    'background-repeat': source.css('backgroundRepeat'),
-                    'background-position': position,
-                    'background-attachment': attachment
-                });
-                if(options.optClass != false) {
-                    glue.addClass(options.optClass);
-                }
-                if(options.draggable) {
+                if(glue.length){
+                    var attachment = source.css('backgroundAttachment');
+                    var position = (attachment == 'fixed') ? '' : '-' + ((glue.offset().left) - (source.offset().left) - (options.offset.x)) + 'px -' + ((glue.offset().top) - (source.offset().top) - (options.offset.y)) + 'px';
                     glue.css({
-                        'background-attachment': 'fixed',
-                        'background-position': '0 0'
+                        'background-image': 'url("' + blurredData + '")',
+                        'background-repeat': source.css('backgroundRepeat'),
+                        'background-position': position,
+                        'background-attachment': attachment
                     });
-                    new DD.Draggable({
-                        node: glue,
-                        cursor: 'move',
-                        move: true
-                    });
+                    if(options.optClass != false) {
+                        glue.addClass(options.optClass);
+                    }
+                    if(options.draggable) {
+                        glue.css({
+                            'background-attachment': 'fixed',
+                            'background-position': '0 0'
+                        });
+                        new DD.Draggable({
+                            node: glue,
+                            cursor: 'move',
+                            move: true
+                        });
+                    }
                 }
-            };
-            Storage.prototype.cacheChecksum = function (opts) {
+            };*/
+            /*Storage.prototype.cacheChecksum = function (opts) {
                 var newData = '';
                 for(var key in opts) {
                     var obj = opts[key];
@@ -126,7 +144,13 @@ KISSY.add(function (S, Node,Base,DD) {
                     console.log('Source Used');
                 }
                 tempImg.src = formattedSource;
-            }
+            }*/
+        },
+        refresh: function(opt){
+            var self = this;
+            ctx.clearRect( 0, 0, canvas.width, canvas.height );
+            ctx.drawImage(tempImg, 0, 0);
+            self._stackBlurCanvasRGB(canvas, 0, 0, canvas.width, canvas.height, opt.radius);
         },
         _stackBlurCanvasRGB: function(id, top_x, top_y, width, height, radius) {
             var self = this;
@@ -367,8 +391,11 @@ KISSY.add(function (S, Node,Base,DD) {
         }
 
     }, {ATTRS : /** @lends Blur*/{
+        node: {
+            value: ''
+        },
         source: {
-            value: 'body'
+            value: ''
         },     //Background to blur
         radius: {
             value: 5
@@ -396,7 +423,7 @@ KISSY.add(function (S, Node,Base,DD) {
         }                //Only used if jQuery UI is present. Will change background-position to fixed
     }});
     return Blur;
-}, {requires:['node', 'base', 'dd']});
+}, {requires:['node', 'base']});
 
 
 
